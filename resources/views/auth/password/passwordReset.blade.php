9506a0596b3607a7ca0bfebbf9355f9fb7cec698
Yonathan-T <itsyonathantaweke@gmail.com>
2025-08-09 21:41:09 +0300

    fix: autofill email field from query string on password reset

--- resources/views/auth/password/passwordReset.blade.php
+++ resources/views/auth/password/passwordReset.blade.php
@@ -5,10 +5,10 @@
 @endif
 <x-auth-layout title="Reset Password" subtitle="Enter your new password below."
     footer='Remembered your password? <a href="/auth/login" class="text-white underline hover:text-[#c6b78e]">Sign in</a>'>
-    <x-auth-form action="/auth/reset-password" button="Reset Password">
+    <x-auth-form action="/auth/login" button="Reset Password">
         {{-- Usually Laravel’s password reset form needs a hidden token --}}
         <input type="hidden" name="token" value="{{ request()->route('token') }}">
-        <x-auth-field name="email" type="email" label="Email" value="{{ old('email') }}" />
+        <x-auth-field name="email" type="email" label="Email" value="{{ old('email') ?? request('email') }}" />
         <x-auth-field name="password" type="password" label="New Password" placeholder="********" />
         <x-auth-field name="password_confirmation" type="password" label="Confirm New Password"
             placeholder="********" />
